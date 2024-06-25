eccc1ca5a0779491c8d4063962f2d3b9e01ca247
t <t2411663@gl.cc.uec.ac.jp>
2024-06-25 18:14:22 +0900

    直した

--- Maru2period/main.js
+++ Maru2period/main.js
@@ -1,12 +1,13 @@
-window.onload = function() {
-function buttonClicj(){
+window.onload = function () {
+function buttonClick(){
     var Text = document.getElementById("input").value;
     Text = Text.replace("。",".");
     Text = Text.replace("，",",");
-};
+    var blob=new Blob([Text],{type:"text/plain"});
+    const link=document.createElement("a");
+    link.href=URL.createObjectURL(blob);
+    link.download="document.tex";
+    link.click();
+    };
 document.getElementById("button").addEventListener("click",buttonClick);
-var blob=new Blob([Text],{type:"text/plain"});
-const link=document.createElement("a");
-link.href=URL.createObjectURL(blob);
-link.download="document.tex";
-link.click();};
+};
