d1e84ae497de4d4e8e36341d303537ac1c4f68a8
t <t2411663@gl.cc.uec.ac.jp>
2024-06-25 17:59:25 +0900

    。を.に変換します

---
+++ Maru2period/main.js
@@ -0,0 +1,10 @@
+document.getElementById("button").onclick=function(){
+    var Text = document.getElementById("input").value;
+    Text = Text.replace("。",".");
+    Text = Text.replace("，",",");
+};
+var blob=new Blob([Text],{type:"text/plain"});
+const link=document.createElement("a");
+link.href=URL.createObjectURL(blob);
+link.download="document.tex";
+link.click();
