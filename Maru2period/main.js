afc5de4f88541fbc258d3a021a33b3edba74dab0
t <t2411663@gl.cc.uec.ac.jp>
2024-06-25 18:08:47 +0900

    Windowがロードされてから実行

--- Maru2period/main.js
+++ Maru2period/main.js
@@ -1,3 +1,4 @@
+window.onload = function() {
 function buttonClicj(){
     var Text = document.getElementById("input").value;
     Text = Text.replace("。",".");
@@ -8,4 +9,4 @@ var blob=new Blob([Text],{type:"text/plain"});
 const link=document.createElement("a");
 link.href=URL.createObjectURL(blob);
 link.download="document.tex";
-link.click();
+link.click();};
